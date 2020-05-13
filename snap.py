b545a2daf04276d828e39fe7dce63f178efeae91
coronoa <coronoa@posteo.de>
2020-05-13 19:23:38 +0200

    dissect wayback url string

--- snap.py
+++ snap.py
@@ -2,7 +2,8 @@ import requests
 
 
 class SnapWaybackVersions:
-    wayback_url = 'https://web.archive.org/cdx/search?url=https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Steckbrief.html&output=json'
+    source_url = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Steckbrief.html'
+    wayback_url = 'https://web.archive.org/cdx/search?url={source_url}&output=json'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'}
 
     def __init__(self):
@@ -11,7 +12,7 @@ class SnapWaybackVersions:
             print(version[1])
 
     def get_versions_data(self):
-        wayback_json = requests.get(self.wayback_url, headers=self.headers).json()
+        wayback_json = requests.get(self.wayback_url.format(source_url=self.source_url), headers=self.headers).json()
         del wayback_json[0]
         return wayback_json
 
